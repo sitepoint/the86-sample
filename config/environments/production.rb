ca3b2691e8971723d407ba5bb19dbbd774ed1ef3
Paul Annesley <paul@annesley.cc>
2012-07-27 22:01:59 +1000

    Disabled ActiveRecord, ActionMailer, ActiveResource, Sprockets.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -12,13 +12,13 @@ The86Sample::Application.configure do
   config.serve_static_assets = false
 
   # Compress JavaScripts and CSS
-  config.assets.compress = true
+  #config.assets.compress = true
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
-  config.assets.compile = false
+  #config.assets.compile = false
 
   # Generate digests for assets URLs
-  config.assets.digest = true
+  #config.assets.digest = true
 
   # Defaults to nil and saved in location specified by config.assets.prefix
   # config.assets.manifest = YOUR_PATH
