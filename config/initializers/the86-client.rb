d27b2711e02187157d961069f084b53853e915fd
Paul Annesley <paul@annesley.cc>
2012-07-27 22:23:12 +1000

    Initializer for the86-client.

---
+++ config/initializers/the86-client.rb
@@ -0,0 +1,8 @@
+# The domain, and optionally port.
+The86::Client.domain = "localhost:3000"
+
+# The HTTP basic auth username and password for the API client.
+The86::Client.credentials = ["sample", "sample"]
+
+# Never do this.
+The86::Client.disable_https!
