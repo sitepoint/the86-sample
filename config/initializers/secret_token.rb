69e16a33f77035a1d1e89d3f87bb36a687274d25
Paul Annesley <paul@annesley.cc>
2012-07-27 21:55:38 +1000

    Initialized new Rails app.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+The86Sample::Application.config.secret_token = 'e22e3d47621799cc82bdd8581b4db938052efe6884f1a848fd212dad789def106ebeda690e16f8a27b980ff800f2538b60b77822ca82cedc7bdd486888455fe4'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+The86Sample::Application.config.session_store :cookie_store, key: '_the86-sample_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# The86Sample::Application.config.session_store :active_record_store
